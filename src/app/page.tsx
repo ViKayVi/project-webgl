4f1c1e9da5952df266bb0abe88d1b63acbb524a8
VKrafftV <victor@anything.so>
2025-10-27 16:19:27 +0100

    Refactor ThreeScene component to utilize Sketch class for improved structure and performance. Update dev script in package.json and add new file for Sketch class. Modify page styles for better visual presentation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ThreeScene from "@/components/ThreeScene";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen bg-sky-400">
       <ThreeScene />
     </div>
   );
--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -1,52 +1,22 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/Addons.js";
+import Sketch from "@/classes/Sketch";
 
 export default function ThreeScene() {
-  const mountRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!mountRef.current) return;
+    if (!containerRef.current) return;
 
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    const scene = new THREE.Scene();
-
-    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-    camera.position.z = 1;
-
-    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    const material = new THREE.MeshNormalMaterial();
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(width, height);
-    mountRef.current.appendChild(renderer.domElement);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.05;
-
-    const animate = (time: number) => {
-      mesh.rotation.x = time / 2000;
-      mesh.rotation.y = time / 1000;
-
-      controls.update();
-      renderer.render(scene, camera);
-    };
-
-    renderer.setAnimationLoop(animate);
+    const sketch = new Sketch({ domElement: containerRef.current });
 
     return () => {
-      renderer.dispose();
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      mountRef.current?.removeChild(renderer.domElement);
+      containerRef.current?.removeChild(sketch.renderer.domElement);
+      sketch.renderer.setAnimationLoop(null);
     };
   }, []);
 
-  return <div ref={mountRef} />;
+  return <div ref={containerRef} style={{ width: "100%", height: "100vh" }} />;
 }
